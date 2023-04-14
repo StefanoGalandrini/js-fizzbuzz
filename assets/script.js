ffab491d695a6cff4ec4ad65105906d8fc3092ea
StefanoGalandrini <stefe68@gmail.com>
2023-04-14 16:55:23 +0200

    Add working html, css and js

--- assets/script.js
+++ assets/script.js
@@ -29,19 +29,39 @@ for (let i = 1; i <= 100; i++) {
 }
 */
 
+// Global variables
 let fizzBuzz = "";
+let typeOfClass = "";
+const square = document.querySelector("main");
 
+// Loop from 1 to 100 included
 for (let i = 1; i <= 100; i++) {
+	// declare target element variable
+	let nextDiv = document.createElement("div");
+
+	// conditions to create inner text
 	if (i % 3 === 0) {
 		fizzBuzz = "Fizz";
+		typeOfClass = "fizz";
 	}
 	if (i % 5 === 0) {
 		fizzBuzz = fizzBuzz + "Buzz";
+		typeOfClass = typeOfClass + "buzz";
 	}
 	if (fizzBuzz === "") {
-		console.log(i);
+		nextDiv.innerHTML = i.toString();
+		nextDiv.classList.add("all", "normal");
+		// console.log(i);
 	} else {
-		console.log(fizzBuzz);
-		fizzBuzz = "";
+		nextDiv.innerHTML = fizzBuzz;
+		nextDiv.classList.add("all", typeOfClass);
+		// console.log(fizzBuzz, typeOfClass);
 	}
+
+	// Create new <div>
+	square.appendChild(nextDiv);
+
+	// Reset variables
+	fizzBuzz = "";
+	typeOfClass = "";
 }
