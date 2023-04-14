03afddd23b448ad75eac4ee01a8edd4a3d3a8054
StefanoGalandrini <stefe68@gmail.com>
2023-04-14 14:47:34 +0200

    Add working javascript with nested IFs

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,29 @@
 /*
-	FIZZBUZZ
+	FIZZBUZZ PSEUDOCODE
+
+	Prepara una variabile fizzBuzz in cui inserire il testo da visualizzare:
+
+	Esegui un ciclo da 1 a 100:
+	FOR (i=1; i<=100; i++)
+
+		fizzBuzz = ""
+		IF resto(i/3) = 0 allora fizzBuzz = "Fizz"
+		IF resto(i/5) = 0 allora fizzBuzz = fizzBuzz + "Buzz"
+
+		scrivi fizzBuzz
 */
+
+/*	cicli IF nidificati:
+	TODO semplificare codice */
+
+for (let i = 1; i <= 100; i++) {
+	if (i % 3 === 0 && i % 5 === 0) {
+		console.log("fizzbuzz");
+	} else if (i % 3 === 0) {
+		console.log("fizz");
+	} else if (i % 5 === 0) {
+		console.log("buzz");
+	} else {
+		console.log(i);
+	}
+}
